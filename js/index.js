5900fb9511c2c6a82371ccf0885b4f3655029939
yejichun123 <yejichun@naver.com>
2020-05-08 18:18:49 +0900

    5/8 작업

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ $(".navi").hover(function(){
 },function(){
 	$(this).find(".navi-subs").stop().slideUp(300);
 });
+
+$(".navi").click(function(){
+	
+	$(this).css("color", "black")
+	$(this).find(".navi-wrap").css("color", "#777777")
+})
