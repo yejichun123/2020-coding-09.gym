b9df69401d4ed6764879925a4ad04c545f07dbcb
yejichun123 <yejichun@naver.com>
2020-05-18 14:48:49 +0900

    lines 작업중

--- js/enfold.js
+++ js/enfold.js
@@ -1,7 +1,9 @@
 // $(".navi-wrap > .navi")
-// $(".navi-wrap").children(".navi")
+// $(".navi-wrap").childrend(".navi")
 $(".navi-wrap").find(".navi").hover(function(){
 	$(this).find(".subs").stop().fadeIn(300);
+	$(this).find(".bar").addClass("active");
 }, function(){
 	$(this).find(".subs").stop().fadeOut(300);
+	$(this).find(".bar").removeClass("active");
 });
