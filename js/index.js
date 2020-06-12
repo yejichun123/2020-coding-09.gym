2a7e02eed9b71424c819a8c90bbcf6b42251f33c
yejichun123 <yejichun@naver.com>
2020-06-12 14:26:27 +0900

    animation 질문

--- js/index.js
+++ js/index.js
@@ -43,20 +43,17 @@ var last = $ban.length - 1;
 
 function onMainNext(){
 	$(".banner1-wrap > .ban2").stop().animate({"top": 0 + "%"}, 500, function(){
-		$(this).removeClass("ban2").addClass("ban1");
-		$(".ban3").removeClass("ban3").addClass("ban2");
-		// $(this).find(".main-title").stop().animate({"transform": "translateX(0%)"}, 300);
-		// $(this).find(".main-subtitle").stop().animate({"transform": "translateX(0%)"}, 200);
+	
 	});
 	$(".banner1-wrap > .ban1").stop().animate({"top": 100 + "%"}, 1000, function(){
-		$(".ban4").removeClass("ban4").addClass("ban3");
-		$(this).removeClass("ban1").addClass("ban4");
-	});
 
-	// for (var i=0, html; i<=last; i++) {
-	// 	if(i >= 1) $(".ban").eq(i) $(".ban").eq(i).removeClass("ban(i)").addClass("ban(i-1)")
-	// 	else(i == 0) $(".ban").eq(i).removeClass("ban(i)").addClass("ban(last)")
-	// }
+	});
+	// setTimeout (function(){
+	// 	$(".banner1-wrap > .ban2").removeClass("ban2").addClass("ban1");
+	// 	$(".banner1-wrap > .ban3").removeClass("ban3").addClass("ban2");
+	// 	$(".banner1-wrap > .ban4").removeClass("ban4").addClass("ban3");
+	// 	$(".banner1-wrap > .ban1").removeClass("ban1").addClass("ban4");
+	// }, 500);
 }
 
 function onMainPrev(){
